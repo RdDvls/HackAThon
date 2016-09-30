e00c3397be9d7dd236e195a838fa52d603549028
Joe Fabiano <jfabiano@gmail.com>
2016-09-30 15:28:19 -0400

    changed the environment variable

--- src/main/java/com/tiy/hack/HackAThonApplication.java
+++ src/main/java/com/tiy/hack/HackAThonApplication.java
@@ -1,6 +1,6 @@
 package com.tiy.hack;
 
-import org.h2.tools.Server;
+//import org.h2.tools.Server;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,10 +13,7 @@ public class HackAThonApplication {
 
 	public static void main(String[] args) throws SQLException {
 		SpringApplication.run(HackAThonApplication.class, args);
-		Server.createWebServer().start();
-	}
-	public void createUser()
-	{
 
 	}
+
 }
